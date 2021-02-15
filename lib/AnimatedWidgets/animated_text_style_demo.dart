adbff335c7ec9eb5973358fabffa58d4376a7876
junjie <yjj950406@gmail.com>
2021-02-14 21:40:22 -0500

    added animated default text style

---
+++ lib/AnimatedWidgets/animated_text_style_demo.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+
+import '../util.dart';
+
+class AnimatedTextStyleDemo extends StatefulWidget {
+  @override
+  _AnimatedTextStyleDemoState createState() => _AnimatedTextStyleDemoState();
+}
+
+class _AnimatedTextStyleDemoState extends State<AnimatedTextStyleDemo> {
+  bool _isAnimated = false;
+  List<String> curveList = List();
+  Curve _curve = Curves.linear;
+  String currentDropDownValue;
+
+  @override
+  void initState() {
+    super.initState();
+    curveList = Util().getSortedKeyList();
+    currentDropDownValue = curveList.first;
+    _curve = Util().curveMap[currentDropDownValue];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        DropdownButton(
+          value: currentDropDownValue,
+          items: curveList
+              .map((value) => DropdownMenuItem<String>(
+                    child: Text(value),
+                    value: value,
+                  ))
+              .toList(),
+          onChanged: (newValue) {
+            setState(() {
+              currentDropDownValue = newValue;
+              _curve = Util().curveMap[currentDropDownValue];
+            });
+          },
+        ),
+        SizedBox(height: 100),
+        AnimatedDefaultTextStyle(
+          child: Text("Flutter is awesome"),
+          duration: Duration(seconds: 3),
+          curve: _curve,
+          style: _isAnimated
+              ? TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.blue,
+                  fontSize: 40,
+                )
+              : TextStyle(
+                  color: Colors.black,
+                  fontSize: 16,
+                ),
+        ),
+        SizedBox(
+          height: 50,
+        ),
+        FlatButton.icon(
+            onPressed: () {
+              setState(() {
+                _isAnimated = !_isAnimated;
+              });
+            },
+            icon: Icon(
+              Icons.play_circle_filled,
+              size: 30,
+              color: Colors.greenAccent,
+            ),
+            label: Text("Click To Play")),
+      ],
+    );
+  }
+}
--- lib/widget_change_notifier.dart
+++ lib/widget_change_notifier.dart
@@ -3,6 +3,7 @@ import 'package:flutter_animation_demo/AnimatedWidgets/animated_container_demo.d
 import 'package:flutter_animation_demo/AnimatedWidgets/animated_crossfade_demo.dart';
 import 'package:flutter_animation_demo/AnimatedWidgets/animated_position_demo.dart';
 import 'package:flutter_animation_demo/AnimatedWidgets/animated_switcher_demo.dart';
+import 'package:flutter_animation_demo/AnimatedWidgets/animated_text_style_demo.dart';
 import 'package:flutter_animation_demo/AnimatedWidgets/tween_animated_demo.dart';
 
 import 'AnimatedWidgets/animated_align_demo.dart';
@@ -15,7 +16,8 @@ class WidgetChangeNotifier extends ChangeNotifier {
     MyAnimatedWidget("AnimatedSwitcher", AnimatedSwitcherDemo()),
     MyAnimatedWidget("AnimatedPositioned", AnimatedPositionDemo()),
     MyAnimatedWidget("AnimatedAlign", AnimatedAlignDemo()),
-    MyAnimatedWidget("AnimatedCrossFade", AnimatedCrossFadeDemo())
+    MyAnimatedWidget("AnimatedCrossFade", AnimatedCrossFadeDemo()),
+    MyAnimatedWidget("AnimatedDefaultTextStyle", AnimatedTextStyleDemo())
   ];
 
   MyAnimatedWidget currentAnimatedWidget;
