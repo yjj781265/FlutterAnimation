3514f71aadbce804a718a22921ec49d0049ba3c7
Junjie Yang <yjj950406@gmail.com>
2021-02-27 14:00:32 -0500

    force app works only in portrait only

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_animation_demo/widget_change_notifier.dart';
 import 'package:provider/provider.dart';
 
 import 'my_animated_widget.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+
   runApp(MyApp());
 }
 
