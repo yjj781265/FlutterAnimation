8c9ccfd342143f5236c349b06f15f4925ad882e2
junjie <yjj950406@gmail.com>
2021-03-07 23:30:26 -0500

    update ad unit ad and app id for admob

--- lib/ad_helper.dart
+++ lib/ad_helper.dart
@@ -5,9 +5,9 @@ import 'package:google_mobile_ads/google_mobile_ads.dart';
 class AdHelper {
   static String get bannerAdUnitId {
     if (Platform.isAndroid) {
-      return 'ca-app-pub-3940256099942544/6300978111';
+      return 'ca-app-pub-7007793257643098/5232583523';
     } else if (Platform.isIOS) {
-      return 'ca-app-pub-3940256099942544/2934735716';
+      return 'ca-app-pub-7007793257643098/6992704981';
     }
     throw new UnsupportedError("Unsupported platform");
   }
